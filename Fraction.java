7ea3fa1fe0c5e46b7df3521f569e0889b30490d2
Consuelo Jimenez <jimenez.m.consuelo@gmail.com>
2015-03-23 11:28:06 -0700

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -2,7 +2,6 @@
  *
  * This class represents a fraction whose numerator and denominator are integers.
  *
- * Requirements:
  *      Implement interfaces: FractionInterface and Comparable (i.e. compareTo())
  *      Implement methods equals() and toString() from class Object
  *
@@ -11,28 +10,8 @@
  *      For number such as 3/-10, it is same as -3/10 (see hints 2. below)
  *      Must display negative fraction as -x/y,
  *         example: (-3)/10 or 3/(-10), must display as -3/10
- *      Must throw excpetion in case of errors
- *      Must not add new or modify existing data fields
- *      Must not add new public methods
- *      May add private methods
+ *      throws excpetion in case of errors
  *
- * Hints:
- *
- * 1. To reduce a fraction such as 4/8 to lowest terms, you need to divide both
- *    the numerator and the denominator by their greatest common denominator.
- *    The greatest common denominator of 4 and 8 is 4, so when you divide
- *    the numerator and denominator of 4/8 by 4, you get the fraction 1/2.
- *    The recursive algorithm which finds the greatest common denominator of
- *    two positive integers is implemnted (see code)
- *
- * 2. It will be easier to determine the correct sign of a fraction if you force
- *    the fraction's denominator to be positive. However, your implementation must
- *    handle negative denominators that the client might provide.
- *
- * 3. You need to downcast reference parameter FractionInterface to Fraction if
- *    you want to use it as Fraction. See add, subtract, multiply and divide methods
- *
- * 4. Use "this" to access this object if it is needed
  *
  ************************************************************************************/
 package PJ1;
